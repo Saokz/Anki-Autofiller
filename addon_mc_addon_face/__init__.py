4e295ecacedfefb17b3bc682334bab30215eb5dc
Saokz <threebearsatonce@gmail.com>
2019-09-20 20:37:53 -0500

    Added template check, and documentation

--- addon_mc_addon_face/__init__.py
+++ addon_mc_addon_face/__init__.py
@@ -1,47 +1,45 @@
-# import the main window object (mw) from aqt
 from aqt import mw
 from anki import notes
-# import the "show info" tool from utils.py
 from aqt.utils import showInfo
-# import all of the Qt GUI library
 from aqt.qt import *
 
-def testFunction():
+def autoFill():
 
-    m = mw.col.models
+    models = mw.col.models
 
-    autoModel = m.new(_("Autofiller"))
-    srchField = m.newField(_("Search Term"))
-    pinDict = m.newField(_("PinoyDict"))
-    tagLess = m.newField(_("TagalogLessons"))
-    diksy = m.newField(_("Diksiyonaryo"))
-    m.addField(autoModel, srchField)
-    m.addField(autoModel, pinDict)
-    m.addField(autoModel, tagLess)
-    m.addField(autoModel, diksy)
+    #Search for the Autofiller model, and grab it
+    m = models.byName("Autofiller")
 
-    autoT = m.newTemplate(_("Autofiller Template"))
-    autoT['qfmt'] = "{{"+_("Search Term")+"}}"
-    autoT['afmt'] = "{{"+_("Search Term")+"}}\n\n<hr id=answer>{{"+_("PinoyDict")+"}}<br>\n"+"{{"+_("TagalogLessons")+"}}<br>\n"+"{{"+_("Diksiyonaryo")+"}}"
+    #if our Autofiller model doesn't exist, create one
+    if m == None:
 
-    m.addTemplate(autoModel, autoT)
-    m.add(autoModel)
+        #create a new model and add it to the list of models
+        m = models.new(_("Autofiller"))
+        models.addField(m, models.newField(_("Search Term") ))
+        models.addField(m, models.newField(_("PinoyDict") ))
+        models.addField(m, models.newField(_("TagalogLessons") ))
+        models.addField(m, models.newField(_("Diksiyonaryo") ))
 
-    #need to add in a condition that checks to see if the autofiller template exists,
-    #and adds it if necessary
+        #Create a card template. 'qfmt' is the front side of the card, 'afmt' the back
+        t = models.newTemplate(_("Autofiller"))
+        t['qfmt'] = "{{"+_("Search Term")+"}}"
+        t['afmt'] = "{{"+_("Search Term")+"}}\n\n<hr id=answer>{{"+_("PinoyDict")+"}}<br>\n"+"{{"+_("TagalogLessons")+"}}<br>\n"+"{{"+_("Diksiyonaryo")+"}}"
 
-    #figured out how to create a note and add information to it
-    n = notes.Note(mw.col, autoModel)
-    n.fields[0] = "bababooey search"
-    n.fields[1] = "bababooey PinoyDict"
-    n.fields[2] = "bababooey TagLessons"
-    n.fields[3] = "bababooey Diksiyonaryo"
+        models.addTemplate(m, t)
+        models.add(m)
+
+    #Create a note with our model and populate it.
+    n = notes.Note(mw.col, m)
+    n.fields[0] = "bababooey search"        #This text is going to be temporary.
+    n.fields[1] = "bababooey PinoyDict"     #This is where we use the webscrapers,
+    n.fields[2] = "bababooey TagLessons"    #and the GUI to get user input for the
+    n.fields[3] = "bababooey Diksiyonaryo"  #search term.
 
     mw.col.addNote(n)
 
 # create a new menu item, "test"
-action = QAction("test", mw)
+action = QAction("Autofiller", mw)
 # set it to call testFunction when it's clicked
-action.triggered.connect(testFunction)
+action.triggered.connect(autoFill)
 # and add it to the tools menu
 mw.form.menuTools.addAction(action)
