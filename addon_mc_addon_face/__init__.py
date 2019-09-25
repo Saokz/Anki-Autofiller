c51acf3ef6d1064b4c41be80ac34d1809cfe946f
Saokz <threebearsatonce@gmail.com>
2019-09-24 22:37:40 -0500

    autofiller now uses tagscrape

--- addon_mc_addon_face/__init__.py
+++ addon_mc_addon_face/__init__.py
@@ -1,11 +1,9 @@
 from aqt import mw
 from anki import notes
-from aqt.utils import showInfo
+#from aqt.utils import showInfo delete later
 from aqt.utils import tooltip
 from aqt.qt import * #change these imports to be more specific
-from addonface import pinoydict
-from addonface import diksiyonaryo
-from addonface import taglessons
+from addonface import tagscrape #change addon folder name
 import threading
 
 def autoFill():
@@ -28,22 +26,22 @@ def autoFill():
         t['qfmt'] = "{{"+_("Search Term")+"}}"
         t['afmt'] = "{{"+_("Search Term")+"}}\n\n<hr id=answer>{{"+_("PinoyDict")+"}}<br>\n"+"{{"+_("TagalogLessons")+"}}<br>\n"+"{{"+_("Diksiyonaryo")+"}}"
 
-        models.addTemplate(m, t)
-        models.add(m)
-
-    pThread = threading.Thread(target=pinoydict.translate("din"))
-    tThread = threading.Thread(target=taglessons.translate("din"))
-    dThread = threading.Thread(target=diksiyonaryo.translate("din"))
-
-    #Create a note with our model and populate it.
+        models.addTemplate(m, t) #necessary?
+        models.add(m) #necessary?
     n = notes.Note(mw.col, m)
-    n.fields[0] = "din"  #This text is going to be temporary.
-    n.fields[1] = pThread.start() #okay so this is a problem. thread.start() doesn't return anything
-    n.fields[2] = tThread.start() #but we want the translate function to be threaded. And we want to
-    n.fields[3] = dThread.start() #store the strings generated into these fields.
 
+    #populate fields of note with the definitions
+    results = tagscrape.translate("din")
+    n.fields[0] = "din"
+    n.fields[1] = results[0]
+    n.fields[2] = results[1]
+    n.fields[3] = results[2]
+    
+    #add note to the collection
     mw.col.addNote(n)
     tooltip("Added autofilled note")
+
+
 
 # create a new menu item, "test"
 action = QAction("Autofiller", mw)
